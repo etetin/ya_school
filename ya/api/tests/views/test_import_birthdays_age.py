dd13660b71ab354c42f593d7dabace6bbf7cf955
Eugene <e.tetin.another@gmail.com>
2019-08-16 11:28:07 +0300

    Create senseless  test for import_birthdays_age().

---
+++ ya/api/tests/views/test_import_birthdays_age.py
@@ -0,0 +1,50 @@
+from rest_framework.test import APITransactionTestCase
+
+from ya.common.models import Citizen
+
+
+class TestImportBirthdaysAge(APITransactionTestCase):
+    fixtures = [
+        'citizens.json'
+    ]
+    
+    IMPORT_ID = 3
+
+    def test_wrong_request_method(self):
+        url = '/imports/1/citizens/birthdays'
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.put(url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 405)
+
+    def test_wrong_url(self):
+        response = self.client.get('/imports/142374/citizens/birthdays/age')
+        self.assertEqual(response.status_code, 404)
+
+    # not best test, but it is what it is
+    def test_request(self):
+        response = self.client.get(f'/imports/{self.IMPORT_ID}/citizens/birthdays/age')
+        expected_data = {
+            'data': [
+                {
+                    'town': 'spb',
+                    'p50': 61.0,
+                    'p75': 86.0,
+                    'p99': 114.8,
+                }
+            ]
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(expected_data, response.data)
+
+
+
+
